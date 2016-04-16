6b5bd78b1038d823892082f835408c9196b26b0f
Igorocky <Igorocky@users.noreply.github.com>
2016-04-17 00:28:58 +0200

    Add editing

--- app/actors/PropsHolderActor.scala
+++ app/actors/PropsHolderActor.scala
@@ -6,8 +6,11 @@ import controllers.IntProp
 object PropsHolderActor {
   def props = Props[PropsHolderActor]
 
-  case class Add(prop: IntProp)
   case class GetAll()
+  case class Add(prop: IntProp)
+  case class Remove(key: String)
+  case class Get(key: String)
+  case class Change(prop: IntProp)
 
   case class Success()
   case class Error(msg: String, props: List[IntProp])
@@ -27,6 +30,26 @@ class PropsHolderActor extends Actor {
       } else {
         props ::= prop
         sender ! Success()
+      }
+    case Change(prop) =>
+      if (!props.exists(_.key == prop.key)) {
+        sender ! Error(s"Property with key='${prop.key}' doesn't exists", props)
+      } else {
+        props = prop :: props.filterNot(_.key == prop.key)
+        sender ! Success()
+      }
+    case Remove(key) =>
+      if (!props.exists(_.key == key)) {
+        sender ! Error(s"Property with key='${key}' doesn't exists", props)
+      } else {
+        props = props.filterNot(_.key == key)
+        sender ! Success()
+      }
+    case Get(key) =>
+      if (!props.exists(_.key == key)) {
+        sender ! Error(s"Property with key='${key}' doesn't exists", props)
+      } else {
+        sender ! props.find(_.key == key).get
       }
   }
 }
--- app/controllers/FormExampleController.scala
+++ app/controllers/FormExampleController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import javax.inject.{Inject, Named, Singleton}
 
-import actors.PropsHolderActor.{Add, Error, GetAll, Success}
+import actors.PropsHolderActor._
 import akka.actor.ActorRef
 import akka.pattern.ask
 import akka.util.Timeout
@@ -11,13 +11,12 @@ import play.api.data.Forms._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc.{Action, Controller}
+import views.html.props._
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-case class IntProp(key: String, value: Int) {
-
-}
+case class IntProp(key: String, value: Int)
 
 object IntProp {
   val KEY = "key"
@@ -39,23 +38,71 @@ class FormExampleController @Inject()(
     )(IntProp.apply)(IntProp.unapply)
   )
 
-  def allProps = Action.async(
-    (propsHolderActor ? GetAll()).map{
-      case list: List[IntProp] @unchecked => Ok(views.html.props(list, propForm))
-    }
+  def allProps = Action(
+    Ok(listProps(getAllProps, propForm, propForm))
   )
 
   def addProp = Action.async(
       parse.form(
         propForm,
-        onErrors = (formWithErrors: Form[IntProp]) => BadRequest(views.html.props(getAllProps, formWithErrors))
+        onErrors = (formWithErrors: Form[IntProp]) => BadRequest(listProps(
+          allProps = getAllProps,
+          addForm = formWithErrors,
+          editForm = propForm
+        ))
       )
   ){request =>
     (propsHolderActor ? Add(request.body)).map{
       case Success() => Redirect(routes.FormExampleController.allProps)
-      case e: Error => BadRequest(views.html.props(e.props, propForm.fill(request.body), Some(e.msg)))
+      case e: Error => BadRequest(listProps(
+        allProps = e.props,
+        addForm = propForm.fill(request.body),
+        editForm = propForm,
+        errMsg = Some(e.msg)
+      ))
+    }
+  }
+
+  def removeProp(key: String) = Action.async{request =>
+    (propsHolderActor ? Remove(key)).map{
+      case Success() => Redirect(routes.FormExampleController.allProps)
+      case e: Error => BadRequest(listProps(
+        allProps = e.props,
+        addForm = propForm,
+        editForm = propForm,
+        errMsg = Some(e.msg)))
+    }
+  }
+
+  def prepareEdit(key: String) = Action{request =>
+    Ok(listProps(
+        allProps = getAllProps,
+        addForm = propForm,
+        editForm = propForm.fill(getProp(key))
+    ))
+  }
+
+  def editProp = Action.async(
+    parse.form(
+      propForm,
+      onErrors = (formWithErrors: Form[IntProp]) => BadRequest(listProps(
+        allProps = getAllProps,
+        addForm = propForm,
+        editForm = formWithErrors
+      ))
+    )
+  ){request =>
+    (propsHolderActor ? Change(request.body)).map{
+      case Success() => Redirect(routes.FormExampleController.allProps)
+      case e: Error => BadRequest(listProps(
+        allProps = e.props,
+        addForm = propForm,
+        editForm = propForm.fill(request.body),
+        errMsg = Some(e.msg)
+      ))
     }
   }
 
   private def getAllProps = Await.result((propsHolderActor ? GetAll()).map(_.asInstanceOf[List[IntProp]]), 5.seconds)
+  private def getProp(key: String) = Await.result((propsHolderActor ? Get(key)).map(_.asInstanceOf[IntProp]), 5.seconds)
 }
